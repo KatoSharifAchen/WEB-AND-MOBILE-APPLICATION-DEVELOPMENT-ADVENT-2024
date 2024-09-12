24d72d09b542da03711097d3255258ffd3522888
KatoSharifAchen <sharifachenkato@gmail.com>
2024-09-12 17:54:42 +0300

    Add files via upload

---
+++ first_js_program.js
@@ -0,0 +1,4 @@
+//  my first js program
+console.log("hello world")
+console.log("kato sharif achen")
+console.log=("B28832")
---
+++ variables.js
@@ -0,0 +1,17 @@
+//Variables
+var my_name= "Kato Sharif Achen"
+// Assign a varriable
+height = 5,8;
+
+console.log(my_name);
+
+console.log(height);
+
+// let
+let area= 78.5
+
+let f_name;
+f_name="good"
+
+console.log(area);
+console.log(f_name);
