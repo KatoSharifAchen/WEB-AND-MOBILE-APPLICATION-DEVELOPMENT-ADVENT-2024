60840542f4e5dd20e69b2c86afb4d3843758b7d7
Kato Sharif Achen <sharifachenkato@gmail.com>
2024-11-09 21:22:45 +0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,12 @@
+let fruit = 'apple';
+
+switch (fruit) {
+  case 'banana':
+    console.log('Bananas are yellow.');
+    break;
+  case 'apple':
+    console.log('Apples are red or green.');
+    break;
+  default:
+    console.log('Unknown fruit.');
+}
