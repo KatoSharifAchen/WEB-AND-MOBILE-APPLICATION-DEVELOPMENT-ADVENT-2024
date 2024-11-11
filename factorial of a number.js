8c093c6ab448aa6196c0710a22733239b1abf69a
Kato Sharif Achen <sharifachenkato@gmail.com>
2024-11-11 15:32:05 +0300

    Add files via upload

---
+++ factorial of a number.js
@@ -0,0 +1,6 @@
+//a function that calculates the factorial of a number.
+function factorial(n) {
+ if (n === 0 || n === 1) return 1;
+ return n * factorial(n - 1);
+}
+console.log(factorial(5)); // 120
